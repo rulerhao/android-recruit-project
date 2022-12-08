eafb830eea183e2362ab701d39829cca18ff9b74
rulerhao <z19970615@gmail.com>
2022-12-09 06:05:51 +0800

    1. Added main activity to project.

---
+++ app/src/main/java/in/hahow/android_recruit_project/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package `in`.hahow.android_recruit_project.view
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
