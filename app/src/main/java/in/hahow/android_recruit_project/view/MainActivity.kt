20ad710d08a3e028cf819cffe12eab8ee2974ed2
rulerhao <z19970615@gmail.com>
2022-12-09 06:28:26 +0800

    1. Added the basic theme for project.

--- app/src/main/java/in/hahow/android_recruit_project/view/MainActivity.kt
+++ app/src/main/java/in/hahow/android_recruit_project/view/MainActivity.kt
@@ -1,5 +1,6 @@
 package `in`.hahow.android_recruit_project.view
 
+import `in`.hahow.android_recruit_project.ui.theme.AppTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,9 +11,8 @@ class MainActivity: ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            // Set the basic theme.
             AppTheme {
-                MainScreen()
-                SystemBarTheme()
             }
         }
     }
