cbc94023d956075badf1fa548c14b5a8a4d56885
rulerhao <z19970615@gmail.com>
2022-12-09 07:04:36 +0800

    1. Added main screen with nothing, just a title.

--- app/src/main/java/in/hahow/android_recruit_project/view/MainActivity.kt
+++ app/src/main/java/in/hahow/android_recruit_project/view/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity: ComponentActivity() {
         setContent {
             // Set the basic theme.
             AppTheme {
+                MainScreen()
             }
         }
     }
---
+++ app/src/main/java/in/hahow/android_recruit_project/view/MainScreen.kt
@@ -0,0 +1,12 @@
+package `in`.hahow.android_recruit_project.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen(
+
+) {
+
+
+
+}
