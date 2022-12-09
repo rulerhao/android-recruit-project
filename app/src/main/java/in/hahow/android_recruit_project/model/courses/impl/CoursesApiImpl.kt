cc3141b8a8b35e33d078f41d28ca624f83bb95e2
rulerhao <z19970615@gmail.com>
2022-12-09 12:44:27 +0800

    1. Added get courses use case.

--- app/src/main/java/in/hahow/android_recruit_project/model/courses/impl/CoursesApiImpl.kt
+++ app/src/main/java/in/hahow/android_recruit_project/model/courses/impl/CoursesApiImpl.kt
@@ -1,19 +1,25 @@
 package `in`.hahow.android_recruit_project.model.courses.impl
 
 import `in`.hahow.android_recruit_project.model.courses.data.Courses
+import `in`.hahow.android_recruit_project.model.courses.data.Data
 import `in`.hahow.android_recruit_project.model.courses.repository.CoursesApiRepository
 import `in`.hahow.android_recruit_project.model.json.JsonHelper
 import android.content.Context
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class CoursesApiImpl(
-    private val context: Context
+    private val context: Context,
+    private val jsonHelper: JsonHelper
 ): CoursesApiRepository {
 
     private val fileName = "data.json"
     private val type = Courses::class.java
 
-    override suspend fun getCourses(): Courses {
-        return JsonHelper().get(context = context, fileName, type) as Courses
+    override suspend fun getCourses(): Flow<List<Data>> {
+        return flow {
+            emit((jsonHelper.get(context, fileName, type) as Courses).data)
+        }
     }
 
 }
--- app/src/main/java/in/hahow/android_recruit_project/model/courses/use_cases/GetCourses.kt
+++ app/src/main/java/in/hahow/android_recruit_project/model/courses/use_cases/GetCourses.kt
@@ -1,4 +1,15 @@
 package `in`.hahow.android_recruit_project.model.courses.use_cases
 
-class GetCourses {
+import `in`.hahow.android_recruit_project.model.courses.repository.CoursesApiRepository
+import kotlinx.coroutines.flow.Flow
+import `in`.hahow.android_recruit_project.model.courses.data.Data
+
+class GetCourses(
+    val repository: CoursesApiRepository
+) {
+
+    suspend operator fun invoke(): Flow<List<Data>> {
+        return repository.getCourses()
+    }
+
 }
