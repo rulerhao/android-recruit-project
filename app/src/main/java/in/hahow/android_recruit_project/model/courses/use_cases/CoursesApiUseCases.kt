361e448d9ef128ec6a22da996763809e723b62bb
rulerhao <z19970615@gmail.com>
2022-12-09 10:02:58 +0800

    1. Added use case for get courses.

---
+++ app/src/main/java/in/hahow/android_recruit_project/model/courses/use_cases/CoursesApiUseCases.kt
@@ -0,0 +1,5 @@
+package `in`.hahow.android_recruit_project.model.courses.use_cases
+
+data class CoursesApiUseCases(
+    val getCourses: GetCourses
+)
---
+++ app/src/main/java/in/hahow/android_recruit_project/model/courses/use_cases/GetCourses.kt
@@ -0,0 +1,4 @@
+package `in`.hahow.android_recruit_project.model.courses.use_cases
+
+class GetCourses {
+}
