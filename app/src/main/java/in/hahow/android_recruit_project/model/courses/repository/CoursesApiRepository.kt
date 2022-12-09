0c17dd1e4004a30b9b4aa070e577cbb8397e71ca
rulerhao <z19970615@gmail.com>
2022-12-09 12:43:08 +0800

    1. Convert return type to flow to make it more expandable like if it's remote api type than we can add not success response.

--- app/src/main/java/in/hahow/android_recruit_project/model/courses/repository/CoursesApiRepository.kt
+++ app/src/main/java/in/hahow/android_recruit_project/model/courses/repository/CoursesApiRepository.kt
@@ -1,9 +1,10 @@
 package `in`.hahow.android_recruit_project.model.courses.repository
 
-import `in`.hahow.android_recruit_project.model.courses.data.Courses
+import `in`.hahow.android_recruit_project.model.courses.data.Data
+import kotlinx.coroutines.flow.Flow
 
 interface CoursesApiRepository {
 
-    suspend fun getCourses(): Courses
+    suspend fun getCourses(): Flow<List<Data>>
 
 }
