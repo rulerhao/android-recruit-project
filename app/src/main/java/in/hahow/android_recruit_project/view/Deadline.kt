39dc5b0288e94fb7e287d115321a353ba7d96711
rulerhao <z19970615@gmail.com>
2022-12-10 15:09:18 +0800

    1. Added icon for countdown time.

--- app/src/main/java/in/hahow/android_recruit_project/view/Deadline.kt
+++ app/src/main/java/in/hahow/android_recruit_project/view/Deadline.kt
@@ -1,13 +1,21 @@
 package `in`.hahow.android_recruit_project.view
 
+import `in`.hahow.android_recruit_project.R
 import `in`.hahow.android_recruit_project.model.courses.data.Data
 import `in`.hahow.android_recruit_project.model.courses.data.Status
 import `in`.hahow.android_recruit_project.view.util.time.TimeHelper
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Deadline(
@@ -27,9 +35,31 @@ fun Deadline(
         Status.SUCCESS -> str
     }
 
-    Text(
+    Row(
         modifier = modifier,
-        color = Color.Gray,
-        text = text
-    )
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (text.isNotBlank()) {
+            Image(
+                modifier = Modifier.size(20.dp),
+                painter = painterResource(id = R.drawable.timelapse),
+                contentDescription = null
+            )
+        }
+        Spacer(modifier = Modifier.size(4.dp))
+        Text(
+            color = Color.Gray,
+            text = text
+        )
+    }
+//    Image(
+//        modifier = modifier,
+//        painter = painterResource(id = R.drawable.timelapse),
+//        contentDescription = null
+//    )
+//    Text(
+//        modifier = modifier,
+//        color = Color.Gray,
+//        text = text
+//    )
 }
