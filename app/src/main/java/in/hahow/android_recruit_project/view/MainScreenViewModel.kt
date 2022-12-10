7c8f0e3ff5c79379ca46480cae95cff9d118ed10
rulerhao <z19970615@gmail.com>
2022-12-10 22:53:24 +0800

    1. Arranged code in view model.

--- app/src/main/java/in/hahow/android_recruit_project/view/MainScreenViewModel.kt
+++ app/src/main/java/in/hahow/android_recruit_project/view/MainScreenViewModel.kt
@@ -2,7 +2,6 @@ package `in`.hahow.android_recruit_project.view
 
 import `in`.hahow.android_recruit_project.model.courses.data.Data
 import `in`.hahow.android_recruit_project.model.courses.use_cases.CoursesApiUseCases
-import android.util.Log
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -11,15 +10,12 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainScreenViewModel @Inject constructor(private val coursesApiUseCases: CoursesApiUseCases):
     ViewModel() {
-
-    val tag = this::class.simpleName
 
     private val _courses = mutableStateListOf<Data>()
     val courses: List<Data> = _courses
@@ -35,11 +31,8 @@ class MainScreenViewModel @Inject constructor(private val coursesApiUseCases: Co
     private suspend fun getCourses() {
         getCoursesJob?.cancel()
         getCoursesJob = coursesApiUseCases.getCourses()
-            .onStart {
-                Log.d(tag, "onStart")
-            }
             .catch { exception ->
-                Log.d(tag, "catch: ${exception.message}")
+                exception.printStackTrace()
             }
             .onEach { courses ->
                 _courses.clear()
