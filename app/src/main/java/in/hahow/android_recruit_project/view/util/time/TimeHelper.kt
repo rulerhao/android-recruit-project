c3222f37356609a38aa4263fcb4b14802f5c0a25
rulerhao <z19970615@gmail.com>
2022-12-10 21:40:56 +0800

    1. Modified if nest for TimeHelper.

--- app/src/main/java/in/hahow/android_recruit_project/view/util/time/TimeHelper.kt
+++ app/src/main/java/in/hahow/android_recruit_project/view/util/time/TimeHelper.kt
@@ -6,7 +6,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 object TimeHelper {
-
+    
     fun getCountDownText(context: Context, dueDate: String?): String {
 
         val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
@@ -17,15 +17,16 @@ object TimeHelper {
             val date: Date? = if (dueDate == null || dueDate.isEmpty()) null
             else format.parse(dueDate)
             val currentDate = Calendar.getInstance().time
-            if (date != null) {
-                val diff = date.time - currentDate.time
-                val diffMinute = diff / (1000 * 60)
-                val diffHour = diffMinute / 60
-                val diffDay = diffHour / 24
-                if (diffMinute > 0) str = context.getString(R.string.count_minute, diffMinute)
-                if (diffHour > 0) str = context.getString(R.string.count_hour, diffHour)
-                if (diffDay > 0) str = context.getString(R.string.count_day, diffDay)
-            }
+
+            if (date == null) return str
+
+            val diff = date.time - currentDate.time
+            val diffMinute = diff / (1000 * 60)
+            val diffHour = diffMinute / 60
+            val diffDay = diffHour / 24
+            if (diffMinute > 0) str = context.getString(R.string.count_minute, diffMinute)
+            if (diffHour > 0) str = context.getString(R.string.count_hour, diffHour)
+            if (diffDay > 0) str = context.getString(R.string.count_day, diffDay)
         } catch(e: Exception) {
             e.printStackTrace()
         }
@@ -42,10 +43,10 @@ object TimeHelper {
             val date: Date? = if (dueDate == null || dueDate.isEmpty()) null
                 else format.parse(dueDate)
             val currentDate = Calendar.getInstance().time
-            if (date != null) {
-                val diff = date.time - currentDate.time
-                if (diff < 0) return true
-            }
+
+            if (date == null) return false
+            val diff = date.time - currentDate.time
+            if (diff < 0) return true
         } catch(e: Exception) {
             e.printStackTrace()
         }
